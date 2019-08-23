670b29516e67fce47d3941a515a7b0b814ab2b9f
roblav <roblav@users.noreply.github.com>
2019-08-23 23:22:26 +0100

    Test file.

--- app/lib/helpers/atosValidResponse.js
+++ app/lib/helpers/atosValidResponse.js
@@ -1,6 +1,87 @@
 
  /* eslint-disable */
- 
+ const pem = require('pem-file')
+ const { parseString } = require('xml2js');
+
+ function rsaPublicKeyPem(modulus_b64, exponent_b64) {
+
+  function prepadSigned(hexStr) {
+      msb = hexStr[0]
+      if (
+          (msb>='8' && msb<='9') || 
+          (msb>='a' && msb<='f') || 
+          (msb>='A'&&msb<='F')) {
+          return '00'+hexStr;
+      } else {
+          return hexStr;
+      }
+  }
+
+  function toHex(number) {
+      var nstr = number.toString(16)
+      if (nstr.length%2==0) return nstr
+      return '0'+nstr
+  }
+
+  // encode ASN.1 DER length field
+  // if <=127, short form
+  // if >=128, long form
+  function encodeLengthHex(n) {
+      if (n<=127) return toHex(n)
+      else {
+          n_hex = toHex(n)
+          length_of_length_byte = 128 + n_hex.length/2 // 0x80+numbytes
+          return toHex(length_of_length_byte)+n_hex
+      }
+  }
+
+  var modulus = new Buffer(modulus_b64,'base64');
+  var exponent = new Buffer(exponent_b64, 'base64');
+
+  var modulus_hex = modulus.toString('hex')
+  var exponent_hex = exponent.toString('hex')
+
+  modulus_hex = prepadSigned(modulus_hex)
+  exponent_hex = prepadSigned(exponent_hex)
+
+  var modlen = modulus_hex.length/2
+  var explen = exponent_hex.length/2
+
+  var encoded_modlen = encodeLengthHex(modlen)
+  var encoded_explen = encodeLengthHex(explen)
+  var encoded_pubkey = '30' + 
+      encodeLengthHex(
+          modlen + 
+          explen + 
+          encoded_modlen.length/2 + 
+          encoded_explen.length/2 + 2
+      ) + 
+      '02' + encoded_modlen + modulus_hex +
+      '02' + encoded_explen + exponent_hex;
+
+  var seq2 = 
+      '30 0d ' +
+        '06 09 2a 86 48 86 f7 0d 01 01 01' +
+        '05 00 ' +
+      '03' + encodeLengthHex(encoded_pubkey.length/2 + 1) +
+      '00' + encoded_pubkey;
+
+  seq2 = seq2.replace(/ /g,'');
+
+  var der_hex = '30' + encodeLengthHex(seq2.length/2) + seq2;
+
+  der_hex = der_hex.replace(/ /g, '');
+
+  var der = new Buffer(der_hex, 'hex');
+  var der_b64 = der.toString('base64');
+
+  var pem = '-----BEGIN PUBLIC KEY-----\n' 
+      + der_b64.match(/.{1,64}/g).join('\n') 
+      + '\n-----END PUBLIC KEY-----\n';
+
+  return pem
+}
+
  module.exports = (xml) => {
   //  console('atosResult', atosResult);
   const select = require('xml-crypto').xpath;
@@ -12,10 +93,22 @@
 
   const signature = select(doc, "/*/*[local-name(.)='Signature' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']")[0]
   const sig = new SignedXml()
+
   sig.keyInfoProvider = new FileKeyInfo('./app/tls/client_public.pem');
+  // let clientPublicPEM;
+  
+  // parseString(xml, (err, result) => {
+  //   cert = result.paymentManualConfirmation.Signature[0].KeyInfo[0].X509Data[0].X509Certificate[0];
+  //   clientPublicPEM = `-----BEGIN CERTIFICATE-----${cert}-----END CERTIFICATE-----`;
+  //   console.log("clientPublicPEM: ", clientPublicPEM);
+  // });
+  // sig.keyInfoProvider = new FileKeyInfo(clientPublicPEM);
+
   sig.loadSignature(signature)
       
   const result = sig.checkSignature(xml)
+
+  console.log('ATOS Valid Response', result);
 
   return result;
 
--- app/routes/processAtosResponse.js
+++ app/routes/processAtosResponse.js
@@ -1,5 +1,6 @@
 const { parseString } = require('xml2js');
 const fs = require('fs');
+const pem = require('pem-file')
 
 const atosValidResponse = require('../lib/helpers/atosValidResponse');
 
@@ -9,12 +10,40 @@ function processAtosPaymentHandler(req, res) {
   // var doc = new dom().parseFromString(xml)
   // const atosResult = Buffer.from(req.body.base64Response, 'base64');
   // const isAtosResultValid = atosValidResponse(atosResult.toString());
+  let clientPublicPEM;
+  
+    // console.log(util.inspect(result, false, null, true));
+  // console.log(JSON.stringify(
+  //   result.paymentManualConfirmation.Signature[0].KeyInfo[0].X509Data[0].X509Certificate[0]
+  // ));
+  parseString(xml, (err, result) => {
+    cert = result.paymentManualConfirmation.Signature[0].KeyInfo[0].X509Data[0].X509Certificate[0];
+    // console.log(cert);
+    // Load certificate in PEM encoding (base64 encoded DER)
+    // const source = Buffer.from(cert)
+    // clientPublicPEM = pem.encode(source, 'CERTIFICATE');
+    clientPublicPEM = 
+`-----BEGIN CERTIFICATE-----
+${cert}
+-----END CERTIFICATE-----`
+    fs.writeFile("./app/tls/test.pem", clientPublicPEM, function(err) {
+      if(err) {
+          return console.log(err);
+      }
+  
+      console.log("The file was saved!");
+  }); 
+    // console.log("clientPublicPEM: ", clientPublicPEM);
+  });
+
   const isAtosResultValid = atosValidResponse(xml);
 
   if (isAtosResultValid) {
     parseString(xml, (err, result) => {
       // console.log(util.inspect(result, false, null, true));
-      console.log(result);
+      // console.log(JSON.stringify(
+      //   result.paymentManualConfirmation.Signature[0].KeyInfo[0].X509Data[0].X509Certificate[0]
+      // ));
       const responseObj = result.paymentManualConfirmation.response[0].$;
       const { responseCode } = responseObj;
       const { transactionReference } = responseObj;
@@ -24,6 +53,8 @@ function processAtosPaymentHandler(req, res) {
       const pounds = amount.slice(0, -2);
       const pence = amount.substr(amount.length - 2);
       const amountInPounds = pounds + '.' + pence;
+
+      console.log(transactionReference);
       
       if (responseCode === '00') {
         // res.redirect(`/my-payments/make-payment-success/${transactionReference}/${amountInPounds}`);
